78bc41f3e8a94100f75bc6f354aaede0422495af
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-07-28 03:12:41 +0800

    added currency filter

---
+++ morphs/currency/index.js
@@ -0,0 +1,24 @@
+import currencies from '../../lib/currencies';
+
+export default (Vue) => {
+    Vue.filter('morph-currency', (value, code, useSymbol) => {
+        return sandwich(value, code, useSymbol);
+    });
+
+    Vue.prototype.$morphCurrency = (value, code, useSymbol) => {
+        return sandwich(value, code, useSymbol);
+    };
+
+    function sandwich(value, code, useSymbol) {
+        let preffix = '';
+        if(!code) code = 'USD';
+        let match = currencies.filter(item => code === item.code);
+        if(match.length === 0) {
+            preffix = '$';
+            console.warn(`Morphling Warn: The provided code '${ code }' does not exist. Replaced with default $ symbol.`);
+        } else {
+            preffix = match[0].symbol;
+        }
+        return `${ preffix }${ value }`;
+    }
+};
