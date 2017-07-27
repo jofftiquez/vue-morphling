a2f73d7b4e2a580e06d0255d45ca2d674b858a98
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-07-28 03:12:33 +0800

    new filters

--- morphs/index.js
+++ morphs/index.js
@@ -1,17 +1,21 @@
 import capitalize from './capitalize';
 import chop from './chop';
+import currency from './currency';
 import date from './date';
 import json from './json';
 import lowercase from './lowercase';
 import reverse from './reverse';
+import sandwich from './sandwich';
 import uppercase from './uppercase';
 
 export default (Vue) => {
     capitalize(Vue);
     chop(Vue);
+    currency(Vue);
     date(Vue);
     json(Vue);
     lowercase(Vue);
     reverse(Vue);
+    sandwich(Vue);
     uppercase(Vue);
 };
