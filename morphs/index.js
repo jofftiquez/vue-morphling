6020eec1f711a112464a231cf9c6255ba985d2d8
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-07-28 13:10:05 +0800

    new filters

--- morphs/index.js
+++ morphs/index.js
@@ -4,8 +4,11 @@ import currency from './currency';
 import date from './date';
 import json from './json';
 import lowercase from './lowercase';
+import placeholder from './placeholder';
+import replace from './replace';
 import reverse from './reverse';
 import sandwich from './sandwich';
+import truncate from './truncate';
 import uppercase from './uppercase';
 
 export default (Vue) => {
@@ -15,7 +18,10 @@ export default (Vue) => {
     date(Vue);
     json(Vue);
     lowercase(Vue);
+    placeholder(Vue);
+    replace(Vue);
     reverse(Vue);
     sandwich(Vue);
+    truncate(Vue);
     uppercase(Vue);
 };
