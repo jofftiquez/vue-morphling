ab4793d95daf9f41534da12e20106de30bab0e39
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-07-28 13:10:31 +0800

    added truncate filter

---
+++ morphs/truncate/index.js
@@ -0,0 +1,16 @@
+export default (Vue) => {
+    Vue.filter('morph-truncate', (value, length) => {
+        return truncate(value, length);
+    });
+
+    Vue.prototype.$morphTruncate = (value, length) => {
+        return truncate(value, length);
+    };
+
+    function truncate(value, length) {
+        if(value.length < length) {
+            console.warn('Morphling Warn: trucate value cannot be greater than string length.')
+        }
+        return `${ value.substr(0, length) }...`
+    }
+};
