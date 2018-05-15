12cdd67c26a5040b1a8f7663df6b383435dae3cf
Joff Tiquez <jofftiquez@gmail.com>
2018-05-15 15:20:15 +0800

    New directive

---
+++ src/directives/highlight/index.js
@@ -0,0 +1,14 @@
+
+export default (Vue) => {
+  Vue.directive('morph-highlight', {
+    bind
+  });
+
+  function bind(el, binding, vnode) {
+    const str = vnode.elm.innerText;
+    const highlightee = binding.value.split('::')[0];
+    const highlighter = binding.value.split('::')[1];
+    const regex = new RegExp(highlightee, 'gi');
+    el.innerHTML = str.replace(regex, `<span style="background-color: ${ highlighter }">${ highlightee }</span>`)
+  }
+};
