8b1008a5f6b304e35e29c1a760087459f3d589d9
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-07-12 21:44:07 +0800

    test: add test for truncate

--- test/filters/truncate.test.js
+++ test/filters/truncate.test.js
@@ -7,8 +7,14 @@ it('adds a $morphTruncate method to the Vue prototype', () => {
   expect(typeof localVue.prototype.$morphTruncate).toBe('function');
 });
 
-it('should truncate a string with elipses', () => {
+it('should truncate a string with ellipsis', () => {
   const localVue = createLocalVue();
   localVue.use(truncate);
   expect(localVue.prototype.$morphTruncate('Leave out all the rest', 11)).toEqual('Leave out a…');
+});
+
+it('should not truncate', () => {
+  const localVue = createLocalVue();
+  localVue.use(truncate);
+  expect(localVue.prototype.$morphTruncate('One more light', 20)).toEqual('One more light');
 });
