2b633fc3bbae830702b5b786648c6da00952ae7d
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:20:42 +0800

    refactor: update import export

--- src/filters/placeholder/index.js
+++ src/filters/placeholder/index.js
@@ -1,14 +1,1 @@
-export const placeholder = (Vue) => {
-  Vue.filter('morph-placeholder', (value, placeholder) => {
-    if (!value) return;
-    return replace(value, placeholder);
-  });
-
-  Vue.prototype.$morphPlaceholder = (value, placeholder) => {
-    return replace(value, placeholder);
-  };
-
-  function replace (value, placeholder) {
-    return placeholder;
-  }
-};
+export * from './placeholder';
