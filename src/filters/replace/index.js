d3a8e2c64259407063913dee8d54e718f02732fb
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:22:45 +0800

    refactor: update import export

--- src/filters/replace/index.js
+++ src/filters/replace/index.js
@@ -1,14 +1,1 @@
-export const replace = (Vue) => {
-  Vue.filter('morph-replace', (value, replacee, replacer) => {
-    if (!value) return;
-    return replace(value, replacee, replacer);
-  });
-
-  Vue.prototype.$morphReplace = (value, replacee, replacer) => {
-    return replace(value, replacee, replacer);
-  };
-
-  function replace (value, replacee, replacer) {
-    return value.replace(replacee, replacer);
-  }
-};
+export * from './replace';
