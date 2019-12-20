a36b887e1f5a33b0fb44082d26f375dc39a06f6c
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:15:56 +0800

    refactor: update import export

---
+++ src/filters/file-size/file-size.js
@@ -0,0 +1,21 @@
+import filesize from 'filesize.js';
+
+export const fileSize = (Vue) => {
+  Vue.filter('morph-file-size', (value) => {
+    if (!value) return;
+    return go(value);
+  });
+
+  Vue.prototype.$morphFileSize = (value) => {
+    return go(value);
+  };
+
+  function go (value) {
+    const num = +value;
+    if (Number.isNaN(num)) {
+      console.warn('Warn: value must be a number.');
+      return;
+    }
+    return filesize(num);
+  }
+};
