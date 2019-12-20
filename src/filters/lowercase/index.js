8e2cf3a0d9d53e177b1ee7ea4b30e5dad780dc4b
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:18:42 +0800

    refactor: udpate import export

--- src/filters/lowercase/index.js
+++ src/filters/lowercase/index.js
@@ -1,15 +1,1 @@
-export const lowerCase = (Vue) => {
-  Vue.filter('morph-lowercase', (value) => {
-    if (!value) return;
-    return lowercase(value);
-  });
-
-  Vue.prototype.$morphLowercase = (value) => {
-    return lowercase(value);
-  };
-
-  function lowercase (value) {
-    if (typeof value !== 'string') value = value.toString();
-    return value.toLowerCase();
-  }
-};
+export * from './lowercase';
