81c2eb4ead686758a95e8ae2e98e834e67ebd611
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:23:33 +0800

    refactor: update import export

--- src/filters/reverse/index.js
+++ src/filters/reverse/index.js
@@ -1,15 +1,1 @@
-export const reverse = (Vue) => {
-  Vue.filter('morph-reverse', (value) => {
-    if (!value) return;
-    return reverse(value);
-  });
-
-  Vue.prototype.$morphReverse = (value) => {
-    return reverse(value);
-  };
-
-  function reverse (value) {
-    if (typeof value !== 'string') value = value.toString();
-    return value.split('').reverse().join('');
-  }
-};
+export * from './reverse';
