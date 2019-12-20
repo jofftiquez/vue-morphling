4f7ad0ebd19b11d624c57aeead53256cd78adc76
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:17:37 +0800

    refactor: update import export

--- src/filters/json/index.js
+++ src/filters/json/index.js
@@ -1,14 +1,1 @@
-export const json = (Vue) => {
-  Vue.filter('morph-json', (value, indent) => {
-    if (!value) return;
-    return json(value, indent);
-  });
-
-  Vue.prototype.$morphJson = (value, indent) => {
-    return json(value, indent);
-  };
-
-  function json (value, indent) {
-    return JSON.stringify(value, null, indent);
-  }
-};
+export * from './json';
