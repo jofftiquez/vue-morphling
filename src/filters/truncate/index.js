15d97f738bde71e209d670bb39a5dc293244b9ef
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-20 15:25:27 +0800

    refactor: update import export

--- src/filters/truncate/index.js
+++ src/filters/truncate/index.js
@@ -1,15 +1,1 @@
-export const truncate = (Vue) => {
-  Vue.filter('morph-truncate', (value, length) => {
-    if (!value) return;
-    return truncate(value, length);
-  });
-
-  Vue.prototype.$morphTruncate = (value, length) => {
-    return truncate(value, length);
-  };
-
-  function truncate (value, length) {
-    if (value.length <= length) return value;
-    return `${value.substr(0, length)}…`;
-  }
-};
+export * from './truncate';
